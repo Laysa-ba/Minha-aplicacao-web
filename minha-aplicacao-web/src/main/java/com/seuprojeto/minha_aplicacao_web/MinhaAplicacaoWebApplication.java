cbd1e025812baded22625c63721ed91c199c857d
Laysa Bittencourt <147190704+Laysa-ba@users.noreply.github.com>
2024-11-19 13:48:46 -0300

    Add files via upload

---
+++ minha-aplicacao-web/src/main/java/com/seuprojeto/minha_aplicacao_web/MinhaAplicacaoWebApplication.java
@@ -0,0 +1,12 @@
+package com.seuprojeto.minha_aplicacao_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaAplicacaoWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinhaAplicacaoWebApplication.class, args);
+    }
+}
